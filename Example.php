b29b5b68bdc5b37de26b3e826d61768ad91562a9
Guy Zuercher <gzuercher@geenux.raptus.local>
2016-10-12 13:56:03 +0200

    Better explanations Add dump method Add SubscriptionsOfCustomer Timezone not global

--- Example.php
+++ Example.php
@@ -11,31 +11,32 @@ include "MonsumSubscription.php";
 include "MonsumInvoice.php";
 include "MonsumSubscriptionsOfCustomer.php";
 
+date_default_timezone_set("Europe/Zurich");
+
 $a = new MonsumAPI(array(MonsumAPI::MONSUM_API_CACHE     => true, 
                          MonsumAPI::MONSUM_API_CACHE_DIR => "../cache", // no trailing slash
                          MonsumAPI::MONSUM_API_CACHE_AGE => 300, // 5 minutes
                          MonsumAPI::MONSUM_API_DEBUG     => false,
                          MonsumAPI::MONSUM_API_LOGFILE   => "../log/monsum_api.log",
-                         MonsumAPI::MONSUM_API_TIMEZONE  => "Europe/Zurich",
                          MonsumAPI::MONSUM_API_URL       => "https://app.monsum.com/api/1.0/api.php",
                          MonsumAPI::MONSUM_API_KEY       => "123",  
                          MonsumAPI::MONSUM_API_EMAIL     => "user@domain.tld"));
 
 $t = new MonsumCustomer($a);
 $t->loadCustomerByID("1234567");
-print $t->getOrganization();
+$t->dump();
 
 $t = new MonsumProduct($a);
 $t->loadProductByID("abc123");
-print $t->getTitle();
+$t->dump();
 
 $t = new MonsumAddon($a);
 $t->loadAddonByID("def123");
-print $t->getTitle();
+$t->dump();
 
 $t = new MonsumSubscription($a);
 $t->loadSubscriptionByID("123456789");
-print $t->getProductNumber();
+$t->dump();
 
 $t = new MonsumSubscriptionsOfCustomer($a, "1234567");
 foreach ($t as $key => $value) {
@@ -44,6 +45,7 @@ foreach ($t as $key => $value) {
 
 $t = new MonsumInvoice($a);
 $t->loadInvoiceByNumber("1234567890");
+$t->dump();
 $t->sendByEMail("some@email.com", "Subject", "Message");
 
 ?>
