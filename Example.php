463dedc7a34781fb6a56c23d8546e3167c54d2b3
GeE <gzuercher@raptus.com>
2016-10-15 10:13:45 +0200

    Improved examples

--- Example.php
+++ Example.php
@@ -2,7 +2,10 @@
 
 die("This is non working example code. Please adapt to your needs");
 
+// HTTPFUL Library
+// http://phphttpclient.com/
 include "../httpful/httpful.phar";
+
 include "MonsumAPI.php";
 include "MonsumCustomer.php";
 include "MonsumProduct.php";
@@ -14,10 +17,10 @@ include "MonsumSubscriptionsOfCustomer.php";
 date_default_timezone_set("Europe/Zurich");
 
 $a = new MonsumAPI(array(MonsumAPI::MONSUM_API_CACHE     => true, 
-                         MonsumAPI::MONSUM_API_CACHE_DIR => "../cache", // no trailing slash
+                         MonsumAPI::MONSUM_API_CACHE_DIR => "./cache", // no trailing slash
                          MonsumAPI::MONSUM_API_CACHE_AGE => 300, // 5 minutes
                          MonsumAPI::MONSUM_API_DEBUG     => false,
-                         MonsumAPI::MONSUM_API_LOGFILE   => "../log/monsum_api.log",
+                         MonsumAPI::MONSUM_API_LOGFILE   => "./monsum_api.log",
                          MonsumAPI::MONSUM_API_URL       => "https://app.monsum.com/api/1.0/api.php",
                          MonsumAPI::MONSUM_API_KEY       => "123",  
                          MonsumAPI::MONSUM_API_EMAIL     => "user@domain.tld"));
